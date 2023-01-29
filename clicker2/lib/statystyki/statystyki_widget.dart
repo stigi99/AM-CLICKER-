e838cacd27d70a888fe9e37bb6b5cf9fdd407288
Andrew Kavecky <120323802+Kimirath@users.noreply.github.com>
2023-01-29 20:00:46 +0100

    Statystyki wyświetlają realne wartości

--- clicker2/lib/statystyki/statystyki_widget.dart
+++ clicker2/lib/statystyki/statystyki_widget.dart
@@ -1,3 +1,4 @@
+import '../backend/api_requests/api_calls.dart';
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
 import '../flutter_flow/flutter_flow_widgets.dart';
@@ -13,7 +14,26 @@ class StatystykiWidget extends StatefulWidget {
 }
 
 class _StatystykiWidgetState extends State<StatystykiWidget> {
+  ApiCallResponse? apiResultm7t;
+  TextEditingController? textController1;
+  TextEditingController? textController2;
+  final _unfocusNode = FocusNode();
   final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    textController1 = TextEditingController();
+    textController2 = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _unfocusNode.dispose();
+    textController1?.dispose();
+    textController2?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +62,7 @@ class _StatystykiWidgetState extends State<StatystykiWidget> {
       ),
       body: SafeArea(
         child: GestureDetector(
-          onTap: () => FocusScope.of(context).unfocus(),
+          onTap: () => FocusScope.of(context).requestFocus(_unfocusNode),
           child: ListView(
             padding: EdgeInsets.zero,
             scrollDirection: Axis.vertical,
@@ -68,7 +88,7 @@ class _StatystykiWidgetState extends State<StatystykiWidget> {
                     child: Padding(
                       padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
                       child: Text(
-                        '0',
+                        FFAppState().ectsclicked.toString(),
                         style: FlutterFlowTheme.of(context).bodyText1.override(
                               fontFamily: 'Poppins',
                               fontSize: 20,
@@ -99,7 +119,7 @@ class _StatystykiWidgetState extends State<StatystykiWidget> {
                     child: Padding(
                       padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
                       child: Text(
-                        '0',
+                        FFAppState().ectsaktualne.toString(),
                         style: FlutterFlowTheme.of(context).bodyText1.override(
                               fontFamily: 'Poppins',
                               fontSize: 20,
@@ -130,7 +150,7 @@ class _StatystykiWidgetState extends State<StatystykiWidget> {
                     child: Padding(
                       padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
                       child: Text(
-                        '0',
+                        FFAppState().ectsaktualne.toString(),
                         style: FlutterFlowTheme.of(context).bodyText1.override(
                               fontFamily: 'Poppins',
                               fontSize: 20,
@@ -161,7 +181,7 @@ class _StatystykiWidgetState extends State<StatystykiWidget> {
                     child: Padding(
                       padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
                       child: Text(
-                        '0',
+                        FFAppState().Pskoin.toString(),
                         style: FlutterFlowTheme.of(context).bodyText1.override(
                               fontFamily: 'Poppins',
                               fontSize: 20,
@@ -192,7 +212,7 @@ class _StatystykiWidgetState extends State<StatystykiWidget> {
                     child: Padding(
                       padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
                       child: Text(
-                        '0',
+                        FFAppState().iloscnadgodzin.toString(),
                         style: FlutterFlowTheme.of(context).bodyText1.override(
                               fontFamily: 'Poppins',
                               fontSize: 20,
@@ -223,7 +243,7 @@ class _StatystykiWidgetState extends State<StatystykiWidget> {
                     child: Padding(
                       padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
                       child: Text(
-                        '0',
+                        FFAppState().osobywkadrze.toString(),
                         style: FlutterFlowTheme.of(context).bodyText1.override(
                               fontFamily: 'Poppins',
                               fontSize: 20,
@@ -254,7 +274,7 @@ class _StatystykiWidgetState extends State<StatystykiWidget> {
                     child: Padding(
                       padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
                       child: Text(
-                        '0',
+                        FFAppState().ilosculepszen.toString(),
                         style: FlutterFlowTheme.of(context).bodyText1.override(
                               fontFamily: 'Poppins',
                               fontSize: 20,
@@ -285,7 +305,7 @@ class _StatystykiWidgetState extends State<StatystykiWidget> {
                     child: Padding(
                       padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
                       child: Text(
-                        '0',
+                        FFAppState().iloscodrodzen.toString(),
                         style: FlutterFlowTheme.of(context).bodyText1.override(
                               fontFamily: 'Poppins',
                               fontSize: 20,
@@ -298,34 +318,246 @@ class _StatystykiWidgetState extends State<StatystykiWidget> {
               Row(
                 mainAxisSize: MainAxisSize.max,
                 children: [
-                  Align(
-                    alignment: AlignmentDirectional(0.15, 0.15),
-                    child: Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(240, 250, 0, 0),
-                      child: FFButtonWidget(
-                        onPressed: () async {
-                          context.pushNamed('HomePage');
-                        },
-                        text: 'Wróć',
-                        options: FFButtonOptions(
-                          width: 130,
-                          height: 40,
-                          color: Color(0xFFF1F8F8),
-                          textStyle:
-                              FlutterFlowTheme.of(context).subtitle2.override(
-                                    fontFamily: 'Poppins',
-                                    color: Color(0xFF66B2FF),
-                                  ),
-                          borderSide: BorderSide(
-                            color: Color(0xFF66B2FF),
-                            width: 3,
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(120, 0, 0, 0),
+                    child: Text(
+                      'Wyślij swój wynik',
+                      style: FlutterFlowTheme.of(context).bodyText1,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      child: Align(
+                        alignment: AlignmentDirectional(-0.95, 0),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.45,
+                          child: TextFormField(
+                            controller: textController1,
+                            autofocus: true,
+                            obscureText: false,
+                            decoration: InputDecoration(
+                              hintText: 'Imie',
+                              hintStyle: FlutterFlowTheme.of(context).bodyText2,
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color(0x00000000),
+                                  width: 1,
+                                ),
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(4.0),
+                                  topRight: Radius.circular(4.0),
+                                ),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color(0x00000000),
+                                  width: 1,
+                                ),
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(4.0),
+                                  topRight: Radius.circular(4.0),
+                                ),
+                              ),
+                              errorBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color(0x00000000),
+                                  width: 1,
+                                ),
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(4.0),
+                                  topRight: Radius.circular(4.0),
+                                ),
+                              ),
+                              focusedErrorBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Color(0x00000000),
+                                  width: 1,
+                                ),
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(4.0),
+                                  topRight: Radius.circular(4.0),
+                                ),
+                              ),
+                              filled: true,
+                              fillColor: Color(0xFF9A8F8F),
+                            ),
+                            style: FlutterFlowTheme.of(context).bodyText1,
+                            textAlign: TextAlign.start,
                           ),
-                          borderRadius: BorderRadius.circular(15),
                         ),
+                      ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        controller: textController2,
+                        autofocus: true,
+                        obscureText: false,
+                        decoration: InputDecoration(
+                          hintText: 'Liczba ECTS',
+                          hintStyle: FlutterFlowTheme.of(context).bodyText2,
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(4.0),
+                              topRight: Radius.circular(4.0),
+                            ),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(4.0),
+                              topRight: Radius.circular(4.0),
+                            ),
+                          ),
+                          errorBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(4.0),
+                              topRight: Radius.circular(4.0),
+                            ),
+                          ),
+                          focusedErrorBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color(0x00000000),
+                              width: 1,
+                            ),
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(4.0),
+                              topRight: Radius.circular(4.0),
+                            ),
+                          ),
+                          filled: true,
+                          fillColor: Color(0xFF9A8F8F),
+                        ),
+                        style: FlutterFlowTheme.of(context).bodyText1,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(90, 10, 0, 0),
+                    child: FFButtonWidget(
+                      onPressed: () async {
+                        apiResultm7t = await WyslijWynikCall.call(
+                          title: textController1!.text,
+                          liczba: textController2!.text,
+                        );
+                        if ((apiResultm7t?.succeeded ?? true)) {
+                          await showDialog(
+                            context: context,
+                            builder: (alertDialogContext) {
+                              return AlertDialog(
+                                title: Text('Powiadomienie'),
+                                content: Text('Udało się wysłać  wynik'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(alertDialogContext),
+                                    child: Text('Ok'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        } else {
+                          await showDialog(
+                            context: context,
+                            builder: (alertDialogContext) {
+                              return AlertDialog(
+                                title: Text('Powiadomienie'),
+                                content: Text('Nie udało się wysłać  wyniku'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(alertDialogContext),
+                                    child: Text('Ok'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        }
+
+                        setState(() {});
+                      },
+                      text: 'Wyślij wynik',
+                      options: FFButtonOptions(
+                        width: 200,
+                        height: 40,
+                        color: Color(0xFF66B2FF),
+                        textStyle:
+                            FlutterFlowTheme.of(context).subtitle2.override(
+                                  fontFamily: 'Poppins',
+                                  color: Colors.white,
+                                ),
+                        borderSide: BorderSide(
+                          color: Colors.transparent,
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
                       ),
                     ),
                   ),
                 ],
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Align(
+                        alignment: AlignmentDirectional(0.15, 0.15),
+                        child: Padding(
+                          padding:
+                              EdgeInsetsDirectional.fromSTEB(240, 50, 0, 0),
+                          child: FFButtonWidget(
+                            onPressed: () async {
+                              context.pushNamed('tt');
+                            },
+                            text: 'Wróć',
+                            options: FFButtonOptions(
+                              width: 130,
+                              height: 40,
+                              color: Color(0xFFF1F8F8),
+                              textStyle: FlutterFlowTheme.of(context)
+                                  .subtitle2
+                                  .override(
+                                    fontFamily: 'Poppins',
+                                    color: Color(0xFF66B2FF),
+                                  ),
+                              borderSide: BorderSide(
+                                color: Color(0xFF66B2FF),
+                                width: 3,
+                              ),
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
